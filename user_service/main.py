2a808dc4706bcd410d883dcaecba0b25fffc2e52
Abdulsalam <mr.smady@gmail.com>
2022-11-30 11:09:07 +0300

    add auth

--- user_service/main.py
+++ user_service/main.py
@@ -1,11 +1,43 @@
 from jsonschema.exceptions import ValidationError
+from requests_oauthlib import OAuth2Session
 
 from data import users, UserNotFoundException, UserAlreadyExistsException
-from flask import Flask, request
+from flask import Flask, request, redirect, jsonify
+
+# https://github.com/settings/applications/new
+client_id = "<client_id>"
+client_secret = "<client_secret>"
+
+authorization_base_url = 'https://github.com/login/oauth/authorize'
+token_url = 'https://github.com/login/oauth/access_token'
 
 app = Flask(__name__)
 
 API_BASE = '/api/users'
+
+
+@app.route("/")
+def home_page():
+    github = OAuth2Session(client_id)
+    authorization_url, state = github.authorization_url(authorization_base_url)
+
+    print(authorization_url)
+    print(state)
+    return redirect(authorization_url)
+
+
+@app.get('/callback')
+def callback():
+    github = OAuth2Session(client_id)
+    token = github.fetch_token(token_url, client_secret=client_secret,
+                               authorization_response=request.url)
+
+    # github.token = token
+    github = OAuth2Session(client_id, token=token)
+    user_info = github.get('https://api.github.com/user').json()
+    email = user_info['email']
+
+    return jsonify(email)
 
 
 @app.get(API_BASE)
@@ -45,4 +77,5 @@ def delete_user(user_id):
 
 
 if __name__ == '__main__':
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
     app.run()
